f0396a460c6022fde418200ed388c0b2bb1479db
jamal3121 <jamaltwo.3121@gmail.com>
2024-11-13 13:11:07 +0530

    image corrected

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -72,7 +72,7 @@ const Projects = () => {
         </motion.h2>
         <div>
           {PROJECTS.map((project, index) => (
-            <div key={index} className="mb-4 flex flex-wrap lg:justify-center">
+            <div key={index} className="mb-10 flex flex-wrap lg:justify-center">
               <motion.div
                 whileInView={{ opacity: 1, x: 0 }}
                 initial={{ opacity: 0, x: -100 }}
