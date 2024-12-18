1f7055cacd45de25cf9d810919743848026f4622
jamal3121 <jamaltwo.3121@gmail.com>
2024-12-18 21:38:55 +0530

    contact updates

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import profilePic from "../assets/profile.webp";
+import profilePic from "../assets/profile.jpg";
 import { HERO_CONTENT } from "../constants";
 import {motion} from 'framer-motion';
 
